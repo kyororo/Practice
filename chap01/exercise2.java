83c494dbb3997c5bcade0c91bf5a38afc9b451fa
kyororo <kyoroworks@gmail.com>
2018-04-29 20:50:09 +0900

    display your name

---
+++ chap01/exercise2.java
@@ -0,0 +1,9 @@
+class exercise2{
+    public static void main(String args[]){
+        String name = "Your Name";
+       
+        for(int i=0; i< name.length() ; i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
---
+++ chap01/exercise2.md
@@ -0,0 +1,1 @@
+this program is display your name 
