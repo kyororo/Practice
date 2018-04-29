6a8d78c6a391ea06e62bd04b82c60163bfa2ac7b
kyororo <kyoroworks@gmail.com>
2018-04-29 20:02:23 +0900

    test

---
+++ chap01/hello.java
@@ -0,0 +1,6 @@
+    class hello{
+        public static void main(String args[]){
+            System.out.print("new Java program!!"); 
+            System.out.println("output character on the screen"); 
+        }
+    }
